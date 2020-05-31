c56a4c6f9ba98775e4fa6bdfe4eff2cfc982a3f9
Hywel Rees <hjr555@gmail.com>
2020-05-31 17:29:47 +0100

    Initial commit

---
+++ HJR.Email/Configuration/SmtpSettings.cs
@@ -0,0 +1,30 @@
+﻿namespace HJR.Email.Configuration
+{
+    public class SmtpSettings
+    {
+        /// <summary>
+        /// The address of the SMTP server
+        /// </summary>
+        public string Server { get; set; }
+
+        /// <summary>
+        /// The Port for the SMTP server
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// The Username required for authenticating against the SMTP server
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The Password required for authenticating against the SMTP server
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// The email address to be displayed in the 'sent from' field for the recipient
+        /// </summary>
+        public string SentFrom { get; set; }
+    }
+}
---
+++ HJR.Email/SendGridSender.cs
@@ -0,0 +1,30 @@
+﻿using HJR.Email.Configuration;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace HJR.Email
+{
+    public sealed class SendGridSender : IEmailSender
+    {
+        private readonly SmtpSettings _smtpSettings;
+
+        public SendGridSender(IOptions<SmtpSettings> smtpSettings)
+        {
+            _smtpSettings = smtpSettings.Value;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SendGridClient(_smtpSettings.Password);
+            var from = new EmailAddress(_smtpSettings.SentFrom);
+            var to = new EmailAddress(email);
+
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, string.Empty, htmlMessage);
+
+            return client.SendEmailAsync(msg);
+        }
+    }
+}
